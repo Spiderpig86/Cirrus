3a728447e83dcf70af14d41add0b52e5b1e902e2
Stan <slim679975@gmail.com>
2017-01-13 19:14:18 -0500

    Added responsiveness to header css, mobile menu from header, some more style fixes

--- test_res/test.js
+++ test_res/test.js
@@ -1,12 +1,18 @@
 
 // Show / Hide menu when clicked
 $('.has-sub').on('click', function(e) { // Find all with ID
-    $('.dropdown-menu').hide(); // Hide other menus
-    $('.shown').removeClass('shown');
+    $('.dropdown-menu').not(this).hide(); // Hide other menus
+    //$('.shown').removeClass('shown');
     $(this).find('.dropdown-menu').toggle().toggleClass('shown');
 });
 
-$('section').on('click', function(e) { // Hide when clicking section (will modify later)
+$('section').not('#header-btn').on('click', function(e) { // Hide when clicking section (will modify later)
      $('.dropdown-menu').hide(); // Hide other menus clicking on window
     $('.shown').removeClass('shown');
+    $('#header-menu').removeClass('is-active');
 });
+
+// Show dropdown when clicked
+$('#header-btn').on('click', function(e) {
+    $('#header-menu').toggleClass('is-active');
+})
