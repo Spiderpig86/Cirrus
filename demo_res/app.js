41284bb8b21566a43325b622620b23a4826dbaba
Stan <slim679975@gmail.com>
2017-01-29 21:39:11 -0500

    Dropdown test 2

--- demo_res/app.js
+++ demo_res/app.js
@@ -19,3 +19,16 @@ $(document).ready(function() {
 $('#header-btn').on('click', function(e) {
     $('#header-menu').toggleClass('is-active');
 })
+
+// Show / Hide menu when clicked
+$('.has-sub').on('click', function(e) { // Find all with ID
+    $('.dropdown-menu').not(this).hide(); // Hide other menus
+    //$('.shown').removeClass('shown');
+    $(this).find('.dropdown-menu').toggle().toggleClass('shown');
+});
+
+$('section').not('#header-btn').on('click', function(e) { // Hide when clicking section (will modify later)
+     $('.dropdown-menu').hide(); // Hide other menus clicking on window
+    $('.shown').removeClass('shown');
+    $('#header-menu').removeClass('is-active');
+});
