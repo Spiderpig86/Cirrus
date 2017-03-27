da9eac61a1350dd5b65c0cc570bb5b1adf81924b
Stan <slim679975@gmail.com>
2017-03-26 22:00:54 -0400

    Added gulp.js automation

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+// Automate my builds
+
+var gulp = require('gulp');
+var prop = require('./package.json');
+var minify = require('gulp-minify-css');
+var $ = require('gulp-load-plugins')();
+var head = '\/*\r\n* Cirrus ' + prop.version + '\r\n* Stanley Lim, Copyright 2017\r\n* https://spiderpig86.github.io/Cirrus\r\n*/\r\n';
+
+gulp.task('compile', function() {
+    return gulp.src([
+        './src/animations.css',
+        './src/button.css',
+        './src/card.css',
+        './src/code.css',
+        './src/default.css',
+        './src/font.css',
+        './src/footer.css',
+        './src/forms.css',
+        './src/header.css',
+        './src/layout.css',
+        './src/links.css',
+        './src/lists.css',
+        './src/table.css',
+        './src/util.css'
+    ])
+        .pipe($.concat('cirrus.css'))
+        .pipe($.header(head))
+        .pipe($.size())
+        .pipe(gulp.dest('./bin/'));
+});
+
+gulp.task('minify', ['compile'], function() {
+    return gulp.src(['./bin/cirrus.css'])
+        .pipe(minify())
+        .pipe($.header(head))
+        .pipe($.size())
+        .pipe($.concat('cirrus.min.css'))
+        .pipe(gulp.dest('./bin/'));
+});
