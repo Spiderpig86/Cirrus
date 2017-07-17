2743d798e06021c99d85ee1fa9b00f1e0a08b56a
Stan <slim679975@gmail.com>
2017-07-17 16:21:59 -0400

    Fixed up navigation menu js

--- demo_res/js/app.js
+++ demo_res/js/app.js
@@ -22,14 +22,14 @@ $('#header-btn').on('click', function(e) {
 
 // Show / Hide menu when clicked
 $('.has-sub').on('click', function(e) { // Find all with ID
-    $('.dropdown-menu').not(this).removeClass('dropdown-shown'); // Hide other menus
+    $('.dropdown-menu').not($(this).children('.dropdown-menu')).removeClass('dropdown-shown'); // Hide other menus
     //$('.shown').removeClass('shown');
-    $(this).find('.dropdown-menu').toggleClass('dropdown-shown');
+    $(this).children('.dropdown-menu').toggleClass('dropdown-shown');
 });
 
 $(document).on('click', function(e) { // Hide when clicking section (will modify later)
-    $('.dropdown-menu').not(this).removeClass('dropdown-shown'); // Hide other menus clicking on window
-    $('.dropdown-shown').removeClass('dropdown-shown');
+    $('.dropdown-menu').removeClass('dropdown-shown'); // Hide other menus clicking on window
+    //$('.dropdown-shown').removeClass('dropdown-shown');
     $('#header-menu').removeClass('is-active');
 });
 
@@ -43,9 +43,16 @@ $("#header").click(function(e){
 $('.header-item').not($('.has-sub')).not($('.header-btn')).on('click', function(e) {
     //$('.shown').removeClass('shown');
     $('#header-menu').removeClass('is-active');
+    $('.dropdown-menu').removeClass('dropdown-shown');
 });
 
-$('.dropdown-menu > li').on('click', function(e) {
-    //$('.shown').removeClass('shown');
-    $('#header-menu').removeClass('is-active');
+// $('.dropdown-menu > li').on('click', function(e) {
+//     //$('.shown').removeClass('shown');
+//     $('#header-menu').removeClass('is-active');
+//     $('.dropdown-menu').removeClass('dropdown-shown');
+// });
+
+// Hide menu after clicking menu item
+$('.dropdown-menu li').on('click', function(e) {
+    $(this).removeClass('dropdown-shown');
 });
--- demo_res/js/test.js
+++ demo_res/js/test.js
@@ -1,18 +1,22 @@
 
 // Show / Hide menu when clicked
 $('.has-sub').on('click', function(e) { // Find all with ID
-    $('.dropdown-menu').not(this).removeClass('dropdown-shown'); // Hide other menus
+    $('.dropdown-menu').not($(this).children('.dropdown-menu')).removeClass('dropdown-shown'); // Hide other menus
     //$('.shown').removeClass('shown');
-    $(this).find('.dropdown-menu').toggleClass('dropdown-shown');
+    $(this).children('.dropdown-menu').toggleClass('dropdown-shown');
 });
 
 $('section').not('#header-btn').on('click', function(e) { // Hide when clicking section (will modify later)
     $('.dropdown-menu').removeClass('dropdown-shown'); // Hide other menus clicking on window
-    $('.shown').removeClass('shown');
     $('#header-menu').removeClass('is-active');
 });
 
 // Show dropdown when clicked
 $('#header-btn').on('click', function(e) {
     $('#header-menu').toggleClass('is-active');
-})
+});
+
+// Hide menu after clicking menu item
+$('.dropdown-menu li').on('click', function(e) {
+    $(this).removeClass('dropdown-shown');
+});
