88771950288cb62abd74ac7635cddbaaa194f57a
Stan <slim679975@gmail.com>
2017-08-14 20:15:42 -0400

    Made padding in header only appear at 1003px and above, modified JS in docs to fix issues when dropdown menus are selected, new active color for nav-items

--- demo_res/js/app.js
+++ demo_res/js/app.js
@@ -30,14 +30,16 @@ $('#header-btn').on('click', function(e) {
 // Show / Hide menu when clicked
 $('.has-sub').on('click', function(e) { // Find all with ID
     $('.dropdown-menu').not($(this).children('.dropdown-menu')).removeClass('dropdown-shown'); // Hide other menus
-    //$('.shown').removeClass('shown');
+    $('.has-sub').not($(this)).removeClass('active');
     $(this).children('.dropdown-menu').toggleClass('dropdown-shown');
+    $(this).toggleClass('active');
 });
 
 $(document).on('click', function(e) { // Hide when clicking section (will modify later)
     $('.dropdown-menu').removeClass('dropdown-shown'); // Hide other menus clicking on window
     //$('.dropdown-shown').removeClass('dropdown-shown');
     $('#header-menu').removeClass('active');
+    $('.nav-item').removeClass('active');
 });
 
 /* Clicks within the dropdown won't make
@@ -51,6 +53,7 @@ $('.nav-item').not($('.has-sub')).not($('.header-btn')).on('click', function(e) 
     //$('.shown').removeClass('shown');
     $('.dropdown-menu').removeClass('dropdown-shown');
     $('.nav-btn').not(this).removeClass('active');
+    $(this).removeClass('active');
 });
 
 // $('.dropdown-menu > li').on('click', function(e) {
--- demo_res/js/test.js
+++ demo_res/js/test.js
@@ -2,13 +2,15 @@
 // Show / Hide menu when clicked
 $('.has-sub').on('click', function(e) { // Find all with ID
     $('.dropdown-menu').not($(this).children('.dropdown-menu')).removeClass('dropdown-shown'); // Hide other menus
-    //$('.shown').removeClass('shown');
+    $('.has-sub').not($(this)).removeClass('active');
     $(this).children('.dropdown-menu').toggleClass('dropdown-shown');
+    $(this).toggleClass('active');
 });
 
 $('section').not('#header-btn').on('click', function(e) { // Hide when clicking section (will modify later)
     $('.dropdown-menu').removeClass('dropdown-shown'); // Hide other menus clicking on window
     $('#header-menu').removeClass('active');
+    $('.nav-item').removeClass('active');
 });
 
 // TOGGLE HEADER-NAV
