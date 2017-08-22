b8f7c50ad36a8b40bda22656da25f34eac9e1d02
Stan <slim679975@gmail.com>
2017-08-22 17:57:38 -0400

    chore: Updated to clean-css, updated build scripts for better compression

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 var prop = require('./package.json');
-var minify = require('gulp-minify-css');
+var minify = require('gulp-clean-css');
 var $ = require('gulp-load-plugins')();
 var head = '\/*\r\n* Cirrus ' + prop.version + '\r\n* Stanley Lim, Copyright 2017\r\n* https://spiderpig86.github.io/Cirrus\r\n*/\r\n';
 
@@ -37,7 +37,20 @@ gulp.task('compile', function() {
 
 gulp.task('minify', ['compile'], function() {
     return gulp.src(['./dist/cirrus.css'])
-        .pipe(minify())
+        .pipe(minify({
+            level: {
+                1: {
+                    all: true,
+                    normalizeUrls: false
+                  },
+                2: {
+                    restructureRules: true
+                }
+              }
+        }, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+          }))
         .pipe($.header(head))
         .pipe($.size())
         .pipe($.concat('cirrus.min.css'))
