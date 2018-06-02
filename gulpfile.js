25e730f3564bb67d7f55c39dac72b793a0328091
Stan <slim679975@gmail.com>
2018-06-02 11:08:47 -0400

    chore(gulpfile.js): Updated copyright

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var prop = require('./package.json');
 var minify = require('gulp-clean-css');
 var $ = require('gulp-load-plugins')();
-var head = '\/*\r\n* Cirrus ' + prop.version + '\r\n* Stanley Lim, Copyright 2017\r\n* https://spiderpig86.github.io/Cirrus\r\n*/\r\n';
+var head = '\/*\r\n* Cirrus ' + prop.version + `\r\n* Stanley Lim, Copyright ${(new Date()).getFullYear()}\r\n* https://spiderpig86.github.io/Cirrus\r\n*/\r\n`;
 
 gulp.task('compile', function() {
     return gulp.src('./src/*.css')
