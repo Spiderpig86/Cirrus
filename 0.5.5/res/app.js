b0357a9a454ae8fe0f38d0224c0bcb37297aef89
Spiderpig86 <slim679975@gmail.com>
2018-08-13 21:28:06 -0400

    docs(demo.html): Working on more examples

--- 0.5.5/res/app.js
+++ 0.5.5/res/app.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
 
     if ($(window).scrollTop() >= 120) {
         $('#header').addClass('header-dark');
-        $('#header').removeClass('header-clear');
+        $('#header').removeClass('header-clear-mod');
     } else {
-        $('#header').addClass('header-clear');
+        $('#header').addClass('header-clear-mod');
         $('#header').removeClass('header-dark');
     }
        
    $(window).scroll(function() {
        if ($(window).scrollTop() >= 120) {
            $('#header').addClass('header-dark');
-           $('#header').removeClass('header-clear');
+           $('#header').removeClass('header-clear-mod');
            $('#fade').addClass('faded');
        } else {
-            $('#header').addClass('header-clear');
+            $('#header').addClass('header-clear-mod');
            $('#header').removeClass('header-dark');
            $('#fade').removeClass('faded');
        }
