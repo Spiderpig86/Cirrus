f94f8eff91f77ecf894bc09e34255ebd9201b8fb
Spiderpig86 <slim679975@gmail.com>
2019-06-16 20:06:39 -0700

    :wrench: chore(app.js): Added link to responsiveness docs, updated responsiveness docs, and fixed missing copy link

--- 0.5.5/res/js/app.js
+++ 0.5.5/res/js/app.js
@@ -46,3 +46,43 @@ $('.dropdown-menu li').on('click', function(e) {
     $('#header-menu').removeClass('active');
     $('.nav-btn').removeClass('active');
 });
+
+// Copy doc links
+$('.doc-link').on('click', function(e) {
+    e.stopPropagation();
+    console.log($(this));
+
+    // create hidden text element, if it doesn't already exist
+    var targetId = "_hiddenCopyText_";
+    var origSelectionStart, origSelectionEnd;
+    // must use a temporary form element for the selection and copy
+    target = document.getElementById(targetId);
+    if (!target) {
+        var target = document.createElement("textarea");
+        target.style.position = "absolute";
+        target.style.left = "-9999px";
+        target.style.top = "0";
+        target.id = targetId;
+        document.body.appendChild(target);
+    }
+    target.textContent = $(this).context.href;
+    // select the content
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+    
+    // copy the selection
+    var succeed;
+    try {
+    	  succeed = document.execCommand("copy");
+    } catch(e) {
+        succeed = false;
+    }
+    // restore original focus
+    if (currentFocus && typeof currentFocus.focus === "function") {
+        currentFocus.focus();
+    }
+    // clear temporary content
+    target.textContent = "";
+    return false;
+});
