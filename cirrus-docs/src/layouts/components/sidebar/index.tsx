6ca6f3b31852aa6a4cd97e7b00d7cbfa9a3fe186
Stanley Lim <slim679975@gmail.com>
2020-08-13 01:43:57 -0700

    :sparkles: feat(sidebar.ts): Sidebar items now rendered with config file

--- cirrus-docs/src/layouts/components/sidebar/index.tsx
+++ cirrus-docs/src/layouts/components/sidebar/index.tsx
@@ -1,124 +1,49 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { sidebarConfig } from '../../config/sidebar';
+
 import './index.scss';
 
 export const Sidebar: React.FC<any> = () => {
     return (
         <div className="sidebar px-3">
-            <ul className="menu mb-5">
-                <li className="menu-item">
-                    <div className="menu-addon">
-                        <span className="icon">
-                            <FontAwesomeIcon className="fa-wrapper small" icon={['fas', 'clock']} />
-                        </span>
-                    </div>
-                    <a href="#">
-                        <b>Bold Item</b>
-                    </a>
-                </li>
-                <li className="menu-item">
-                    <div className="menu-addon">
-                        <span className="icon">
-                            <FontAwesomeIcon className="fa-wrapper small" icon={['fas', 'clock']} />
-                        </span>
-                    </div>
-                    <a href="#">
-                        <b>Bold Item</b>
-                    </a>
-                </li>
-                <li className="menu-item">
-                    <div className="menu-addon">
-                        <span className="icon">
-                            <FontAwesomeIcon className="fa-wrapper small" icon={['fas', 'clock']} />
-                        </span>
-                    </div>
-                    <a href="#">
-                        <b>Bold Item</b>
-                    </a>
-                </li>
-                <li className="menu-item">
-                    <div className="menu-addon">
-                        <span className="icon">
-                            <FontAwesomeIcon className="fa-wrapper small" icon={['fas', 'clock']} />
-                        </span>
-                    </div>
-                    <a href="#">
-                        <b>Bold Item</b>
-                    </a>
-                </li>
-                <li className="menu-item">
-                    <div className="menu-addon">
-                        <span className="icon">
-                            <FontAwesomeIcon className="fa-wrapper small" icon={['fas', 'clock']} />
-                        </span>
-                    </div>
-                    <a href="#">
-                        <b>Bold Item</b>
-                    </a>
-                </li>
-            </ul>
+            <ul className="menu mb-3">
+                {sidebarConfig.map((config) => {
+                    const title = config.title ? (
+                        <span className="font-bold uppercase text-gray-500">Getting Started</span>
+                    ) : null;
 
-            <span className="font-bold uppercase text-gray-500">Getting Started</span>
-            <ul className="menu">
-                <li className="menu-item selected">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Basic Item</a>
-                </li>
-            </ul>
+                    const listItems = config.sidebarItems.map((listItemConfig) => {
+                        const glyph = listItemConfig.glyph ? (
+                            <div className="menu-addon">
+                                <span className="icon">
+                                    <FontAwesomeIcon className="fa-wrapper small" icon={listItemConfig.glyph} />
+                                </span>
+                            </div>
+                        ) : (
+                            <></>
+                        );
+
+                        return (
+                            <li className="menu-item">
+                                {glyph}
+                                <a href={listItemConfig.url}>{listItemConfig.text}</a>
+                            </li>
+                        );
+                    });
+
+                    return (
+                        <>
+                            {title}
+                            <ul className="menu mb-3">
+                                {listItems}
+                            </ul>
+                        </>
+                    );
+                })}
+
+                </ul>
         </div>
     );
 };
--- cirrus-docs/src/layouts/config/sidebar.ts
+++ cirrus-docs/src/layouts/config/sidebar.ts
@@ -1,3 +1,132 @@
-export const sidebarMenus = [
-    
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+export interface SidebarItemConfig {
+    fontWeight: string;
+    glyph: IconProp | null;
+    text: string;
+    url: string;
+}
+
+export interface SidebarConfig {
+    title: string;
+    sidebarItems: SidebarItemConfig[];
+}
+
+export const sidebarConfig: SidebarConfig[] = [
+    {
+        title: '',
+        sidebarItems: [
+            {
+                fontWeight: 'bold',
+                glyph: ['fas', 'clock'],
+                text: 'Bold Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'bold',
+                glyph: ['fas', 'clock'],
+                text: 'Bold Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'bold',
+                glyph: ['fas', 'clock'],
+                text: 'Bold Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'bold',
+                glyph: ['fas', 'clock'],
+                text: 'Bold Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'bold',
+                glyph: ['fas', 'clock'],
+                text: 'Bold Item',
+                url: '#'
+            },
+        ],
+    },
+    {
+        title: 'Getting Started',
+        sidebarItems: [
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+        ],
+    },
+    {
+        title: 'Animations',
+        sidebarItems: [
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+        ],
+    },
+    {
+        title: 'Components',
+        sidebarItems: [
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+            {
+                fontWeight: 'normal',
+                glyph: null,
+                text: 'Basic Item',
+                url: '#'
+            },
+        ],
+    },
 ];
