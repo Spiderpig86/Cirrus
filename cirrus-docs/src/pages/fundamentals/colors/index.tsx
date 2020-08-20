f2a63ac03487ffde3b270a45de8ad29cb1b23f73
Stanley Lim <slim679975@gmail.com>
2020-08-19 23:30:05 -0700

    :ledger: docs(color): Added color docs and all its components

--- cirrus-docs/src/pages/fundamentals/colors/index.tsx
+++ cirrus-docs/src/pages/fundamentals/colors/index.tsx
@@ -4,14 +4,54 @@ import { Link } from 'react-router-dom';
 import { TableOfContents } from '../../../layouts/components/toc';
 import { Headline } from '../../../layouts/components/headline';
 import { toc } from './toc';
+import { Display } from './display';
+import { v1, v2Colors, v2 } from './color-config';
 
 export const ColorsPage: React.FC<any> = (props) => {
     return (
         <main className="page-layout">
             <div>
-                <section className="padtop">
+                <section className="padtop" id="colors">
                     <div className="content">
-                        <Headline title="Why Cirrus?" link="#why" />
+                        <Headline title="Colors" link="#colors" />
+                        <div className="divider"></div>
+                        <p>
+                            Cirrus has an extensive color palette that has been updated with <b>0.6.0</b> to make them
+                            more accessible to all page elements.
+                        </p>
+                    </div>
+                </section>
+
+                <section className="padtop" id="v1">
+                    <div className="content">
+                        <Headline title="v1 Colors" link="#v1" />
+                        <div className="divider"></div>
+                        <p>The standard color palette from older versions of the framework.</p>
+
+                        {<Display swatches={v1} />}
+                    </div>
+                </section>
+
+                <section className="padtop" id="v2">
+                    <div className="content">
+                        <Headline title="v2 Colors" link="#v2" />
+                        <div className="divider"></div>
+                        <span className="tag-container group-tags">
+                            <div className="tag tag--dark">Added</div>
+                            <div className="tag tag--info">0.6.0</div>
+                        </span>
+                        <p>
+                            Cirrus has an extensive color palette that has been updated with <b>0.6.0</b> to make them
+                            more accessible to all page elements.
+                        </p>
+                        {v2Colors.map((color) => {
+                            return (
+                                <section id={color}>
+                                    <h6>{color}</h6>
+                                    <Display swatches={v2.get(color)!} />
+                                </section>
+                            );
+                        })}
                     </div>
                 </section>
 
@@ -22,7 +62,7 @@ export const ColorsPage: React.FC<any> = (props) => {
                             <li className="pagination-item pagination-next">
                                 <Link to="../update" className="u-block">
                                     <p className="pagination-item-subtitle">Next</p>
-                                    <h5 className="font-alt font-light m-0">Playground</h5>
+                                    <h5 className="font-alt font-light m-0">Typography</h5>
                                 </Link>
                             </li>
                         </ul>
--- cirrus-docs/src/pages/fundamentals/colors/toc.ts
+++ cirrus-docs/src/pages/fundamentals/colors/toc.ts
@@ -2,13 +2,69 @@ import { TableOfContentsEntry } from '../../../layouts/components/toc';
 
 export const toc: TableOfContentsEntry[] = [
     {
-        name: 'Setup',
-        anchor: '#setup',
+        name: 'Colors',
+        anchor: '#colors',
         children: null,
     },
     {
-        name: 'Project Configuration',
-        anchor: '#project-config',
+        name: 'v1',
+        anchor: '#v1',
         children: null,
+    },
+    {
+        name: 'v2',
+        anchor: '#v2',
+        children: [
+            {
+                name: 'pink',
+                anchor: '#pink',
+                children: null,
+            },
+            {
+                name: 'red',
+                anchor: '#red',
+                children: null,
+            },
+            {
+                name: 'orange',
+                anchor: '#orange',
+                children: null,
+            },
+            {
+                name: 'yellow',
+                anchor: '#yellow',
+                children: null,
+            },
+            {
+                name: 'green',
+                anchor: '#green',
+                children: null,
+            },
+            {
+                name: 'teal',
+                anchor: '#teal',
+                children: null,
+            },
+            {
+                name: 'blue',
+                anchor: '#blue',
+                children: null,
+            },
+            {
+                name: 'indigo',
+                anchor: '#indigo',
+                children: null,
+            },
+            {
+                name: 'purple',
+                anchor: '#purple',
+                children: null,
+            },
+            {
+                name: 'gray',
+                anchor: '#gray',
+                children: null,
+            },
+        ],
     },
 ];
