ff5c149d442d571a8e4ff0d8def4c6038f777a76
Stanley Lim <slim679975@gmail.com>
2020-10-30 00:18:06 -0700

    :sparkles: feat(examples): Added more examples with forms

--- cirrus-docs-next/constants/examples.ts
+++ cirrus-docs-next/constants/examples.ts
@@ -23,6 +23,20 @@ export const DOC_EXAMPLES: ExampleCardData[] = [
         imageUrl: `https://spiderpig86.github.io/Cirrus/0.6.0/docs/examples/login-page/login.png`,
     },
     {
+        title: `Registration Page`,
+        description: `A beautiful registration page.`,
+        tags: [`Forms`, `Frame`, `Hero`, `Buttons`],
+        url: `https://spiderpig86.github.io/Cirrus/0.6.0/docs/examples/registration-page/`,
+        imageUrl: `https://spiderpig86.github.io/Cirrus/0.6.0/docs/examples/registration-page/registration-page.png`,
+    },
+    {
+        title: `Login with Registration Form`,
+        description: `A combined registration and login page.`,
+        tags: [`Forms`, `Frame`, `Hero`, `Buttons`],
+        url: `https://spiderpig86.github.io/Cirrus/0.6.0/docs/examples/register-login-form/`,
+        imageUrl: `https://spiderpig86.github.io/Cirrus/0.6.0/docs/examples/register-login-form/register-login-form.png`,
+    },
+    {
         title: `More coming soon...`,
         description: ``,
         tags: [`Forms`, `Layouts`, `Buttons`],
