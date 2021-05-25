a3a3d11fe5d38be65b831d592af80571fb2c31ff
Stanley Lim <slim679975@gmail.com>
2021-05-25 01:21:06 -0400

    :wrench: chore(manifest.json): Fixing up SEO, added preview image

--- cirrus-docs-next/pages/_app.tsx
+++ cirrus-docs-next/pages/_app.tsx
@@ -68,28 +68,28 @@ const App = ({ Component, pageProps }) => {
                 <meta property="og:url" content="http://stanleylim.me/" />
                 <meta property="og:type" content="article" />
                 <meta property="og:title" content="Stanley Lim" />
-                <meta property="og:image" content="/android-icon-192x192.png" />
+                <meta property="og:image" content="https://cirrus-ui.netlify.app/img/preview.png" />
                 <meta
                     property="og:description"
                     content="The SCSS framework for the modern web. It's component based, customizable, and completely open source."
                 />
                 <meta property="og:site_name" content="Cirrus" />
-                <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-                <link rel="apple-touch-icon" sizes="192x192" href="/android-icon-192x192.png" />
+                <link rel="apple-touch-icon" sizes="57x57" href="https://cirrus-ui.netlify.app//apple-icon-57x57.png" />
+                <link rel="apple-touch-icon" sizes="60x60" href="https://cirrus-ui.netlify.app//apple-icon-60x60.png" />
+                <link rel="apple-touch-icon" sizes="72x72" href="https://cirrus-ui.netlify.app//apple-icon-72x72.png" />
+                <link rel="apple-touch-icon" sizes="76x76" href="https://cirrus-ui.netlify.app//apple-icon-76x76.png" />
+                <link rel="apple-touch-icon" sizes="114x114" href="https://cirrus-ui.netlify.app//apple-icon-114x114.png" />
+                <link rel="apple-touch-icon" sizes="120x120" href="https://cirrus-ui.netlify.app//apple-icon-120x120.png" />
+                <link rel="apple-touch-icon" sizes="144x144" href="https://cirrus-ui.netlify.app//apple-icon-144x144.png" />
+                <link rel="apple-touch-icon" sizes="152x152" href="https://cirrus-ui.netlify.app//apple-icon-152x152.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="https://cirrus-ui.netlify.app//apple-icon-180x180.png" />
+                <link rel="apple-touch-icon" sizes="192x192" href="https://cirrus-ui.netlify.app//android-icon-192x192.png" />
                 <link rel="icon" type="image/png" href="/favicon.ico" />
                 <meta name="msapplication-TileColor" content="#f03d4d" />
-                <meta name="msapplication-TileImage" content="/ms-icon-70x70.png" />
-                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-                <meta name="msapplication-TileImage" content="/ms-icon-150x150.png" />
-                <meta name="msapplication-TileImage" content="/ms-icon-310x310.png" />
+                <meta name="msapplication-TileImage" content="https://cirrus-ui.netlify.app//ms-icon-70x70.png" />
+                <meta name="msapplication-TileImage" content="https://cirrus-ui.netlify.app//ms-icon-144x144.png" />
+                <meta name="msapplication-TileImage" content="https://cirrus-ui.netlify.app//ms-icon-150x150.png" />
+                <meta name="msapplication-TileImage" content="https://cirrus-ui.netlify.app//ms-icon-310x310.png" />
                 <link rel="manifest" href="/manifest.json" />
 
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
