915a770162f7c26f85b04013d32bab993088f004
Stanley Lim <slim679975@gmail.com>
2021-10-09 17:08:57 -0700

    :ledger: docs(0.6.3.md): Update 0.6.3 release README

--- cirrus-docs-next/src/index/index.tsx
+++ cirrus-docs-next/src/index/index.tsx
@@ -44,7 +44,7 @@ const Landing: React.FC<any> = () => {
                             </h5>
 
                             <p className="white">
-                                <b>Version 0.6.2</b> / Gamma IV
+                                <b>Version 0.6.3</b> / Gamma V
                             </p>
 
                             <div className="btn-group">
