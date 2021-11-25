acf619cb0d404d0dd73489fa686659d923e35cab
Stanley Lim <slim679975@gmail.com>
2021-11-25 02:25:09 -0800

    :white_check_mark: test(_functions.spec.scss): Add unit tests for util functions, string-split() now supports multi-char delimiters

---
+++ tests/scss.spec.js
@@ -0,0 +1,13 @@
+// Scss test shim file
+const path = require('path');
+const sassTrue = require('sass-true');
+const glob = require('glob');
+
+const testPath = `tests/**/*.spec.scss`;
+
+describe('Sass', () => {
+    const testFiles = glob.sync(path.resolve(process.cwd(), testPath));
+
+    // Run on each file with describe() and it() functions
+    testFiles.forEach((file) => sassTrue.runSass({ file: file }, { describe, it }));
+});
