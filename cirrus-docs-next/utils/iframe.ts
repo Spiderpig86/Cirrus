52feadc759e7f7001abe0f2efe84975f305cbc9a
Stanley Lim <slim679975@gmail.com>
2022-03-22 02:29:17 -0700

    :wrench: chore(playground.ts): Update CDN links

--- cirrus-docs-next/utils/iframe.ts
+++ cirrus-docs-next/utils/iframe.ts
@@ -1,5 +1,7 @@
+import { PLAYGROUND_ENDPOINT_MAP } from "../constants/playground"
+
 export function wrapContent(code: string): string {
-    return `<link href="https://raw.githack.com/Spiderpig86/Cirrus/master/dist/cirrus-all.min.css" rel="stylesheet" />
+    return `<link href="${PLAYGROUND_ENDPOINT_MAP['LATEST_VERSION']}" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 <body>${code}</body>`;
