b10df00a9e734e83c96892e9fdb703f4d54a6071
Stanley Lim <slim679975@gmail.com>
2022-03-28 01:03:18 -0700

    :wrench: chore(iframe.ts): Fix broken cdn link for start guide page

--- cirrus-docs-next/utils/iframe.ts
+++ cirrus-docs-next/utils/iframe.ts
@@ -1,7 +1,8 @@
 import { PLAYGROUND_ENDPOINT_MAP } from "../constants/playground"
 
 export function wrapContent(code: string): string {
-    return `<link href="${PLAYGROUND_ENDPOINT_MAP['LATEST_VERSION']}" rel="stylesheet" />
+    return `<link href="${PLAYGROUND_ENDPOINT_MAP.get(
+        PLAYGROUND_ENDPOINT_MAP.keys().next().value)}" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 <body>${code}</body>`;
