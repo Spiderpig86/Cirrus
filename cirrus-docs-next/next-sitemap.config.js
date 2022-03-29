c19f23ad4b74f439dfecf4174274af3dc6dfdbe8
Stanley Lim <slim679975@gmail.com>
2022-03-28 23:44:47 -0700

    :wrench: chore(next-sitemap.config.js): Add sitemap generator

---
+++ cirrus-docs-next/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://cirrus-ui.com',
+    generateRobotsTxt: true, // (optional)
+    
+};
