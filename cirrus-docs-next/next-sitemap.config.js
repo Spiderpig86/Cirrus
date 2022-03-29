5be91bf21f87b4ac1d30289f4e1ba50ff7039c5c
Stanley Lim <slim679975@gmail.com>
2022-03-29 00:55:00 -0700

    :wrench: chore(next-sitemap.config.js): Update sitemap config

--- cirrus-docs-next/next-sitemap.config.js
+++ cirrus-docs-next/next-sitemap.config.js
@@ -3,5 +3,14 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://cirrus-ui.com',
     generateRobotsTxt: true, // (optional)
-    
+    changefreq: 'daily',
+    transform: async (config, path) => {
+        return {
+            loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+            changefreq: config.changefreq,
+            priority: config.priority,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+            alternateRefs: config.alternateRefs ?? [],
+        };
+    },
 };
