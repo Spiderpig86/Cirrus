30a334af93b95929fdf53c1004a292107069b5cf
Stanley Lim <slim679975@gmail.com>
2022-03-31 18:09:32 -0700

    Fix bug where imports are not correct within Sass/Scss projects
    - Purged old versions of dependencies and removed unneeded ones
    - Fix import paths which break usage within Sass projects
    - Add missing paths to resolve for unit tests and Gulp builds
    - Verified and tested with new Nextjs project

--- tests/scss.spec.js
+++ tests/scss.spec.js
@@ -9,5 +9,5 @@ describe('Sass', () => {
     const testFiles = glob.sync(path.resolve(process.cwd(), testPath));
 
     // Run on each file with describe() and it() functions
-    testFiles.forEach((file) => sassTrue.runSass({ file: file }, { describe, it }));
+    testFiles.forEach((file) => sassTrue.runSass({ file: file, includePaths: ['node_modules'] }, { describe, it }));
 });
