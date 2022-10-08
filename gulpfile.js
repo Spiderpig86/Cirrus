8ad20517dfa10161ef36ee10ee0a0626b878510d
Stanley Lim <slim679975@gmail.com>
2022-10-08 13:25:47 -0700

    :wrench: chore(gulpfile.js): Add build option for Gzipped versions of Cirrus

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const minify = require('gulp-clean-css');
 const sass = require('gulp-sass')(require('sass')); // Use Dart Sass;
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
+const sizereport = require('gulp-sizereport');
+const gzip = require('gulp-gzip');
 const head =
     '/*\r\n* Cirrus ' +
     prop.version +
@@ -51,46 +53,71 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
     gulp.task(
         `minify-${taskName}`,
         gulp.series(taskName, () => {
-            return gulp
-                .src([`./dist/${outputName}.css`])
-                .pipe(
-                    minify(
-                        {
-                            level: {
-                                1: {
-                                    all: true,
-                                    normalizeUrls: false,
-                                },
-                                2: {
-                                    all: false,
-                                    removeDuplicateRules: true,
-                                    reduceNonAdjacentRules: true,
-                                    removeDuplicateFontRules: true,
-                                    removeDuplicateMediaBlocks: true,
-                                    mergeAdjacentRules: true,
-                                    mergeIntoShorthands: true,
-                                    mergeMedia: true,
-                                    mergeNonAdjacentRules: true,
-                                    mergeSemantically: false,
-                                    removeEmpty: true,
+            return (
+                gulp
+                    .src([`./dist/${outputName}.css`])
+                    .pipe(
+                        minify(
+                            {
+                                level: {
+                                    1: {
+                                        all: true,
+                                        normalizeUrls: false,
+                                    },
+                                    2: {
+                                        all: false,
+                                        removeDuplicateRules: true,
+                                        reduceNonAdjacentRules: true,
+                                        removeDuplicateFontRules: true,
+                                        removeDuplicateMediaBlocks: true,
+                                        mergeAdjacentRules: true,
+                                        mergeIntoShorthands: true,
+                                        mergeMedia: true,
+                                        mergeNonAdjacentRules: true,
+                                        mergeSemantically: false,
+                                        removeEmpty: true,
+                                    },
                                 },
                             },
-                        },
-                        (details) => {
-                            console.log('FULL');
-                            console.log(details.name + ': ' + details.stats.originalSize);
-                            console.log(`${outputName}.min.css ${details.stats.minifiedSize}`);
-                        }
+                            (details) => {
+                                console.log('FULL');
+                                console.log(details.name + ': ' + details.stats.originalSize);
+                                console.log(`${outputName}.min.css ${details.stats.minifiedSize}`);
+                            }
+                        )
                     )
-                )
-                .pipe($.header(head))
-                .pipe($.size())
-                .pipe($.concat(`${outputName}.min.css`))
-                .pipe(gulp.dest('./dist/'))
-                .on('error', (err) => {
-                    console.error(`Error encountered for task ${taskName}. Failing.`);
-                    process.exit(1);
-                });
+                    .pipe($.header(head))
+                    .pipe($.size())
+                    .pipe($.concat(`${outputName}.min.css`))
+                    .pipe(gulp.dest('./dist/'))
+                    .on('error', (err) => {
+                        console.error(`Error encountered for task ${taskName}. Failing.`);
+                        process.exit(1);
+                    })
+            );
+        })
+    );
+
+    // Create Gzipped build
+    gulp.task(
+        `gzip-${taskName}`,
+        gulp.series(taskName, `minify-${taskName}`, () => {
+            return (
+                gulp
+                    .src([`./dist/${outputName}.min.css`])
+                    .pipe(gzip())
+                    .pipe(
+                        sizereport({
+                            gzip: true,
+                            total: false,
+                        })
+                    )
+                    .pipe(gulp.dest('./dist/'))
+                    .on('error', (err) => {
+                        console.error(`Error encountered for task ${taskName}. Failing.`);
+                        process.exit(1);
+                    })
+            );
         })
     );
 }
@@ -98,3 +125,5 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
 gulp.task('watch', () => gulp.watch('./src/**/*.scss', gulp.parallel('minify-ext', 'minify-core', 'minify-all')));
 
 gulp.task('default', gulp.parallel('minify-ext', 'minify-core', 'minify-all'));
+
+gulp.task('gzip', gulp.parallel('gzip-ext', 'gzip-core', 'gzip-all'));
