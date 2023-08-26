39943b26fcf580187de1e3bf54c58b056bec33b0
Stanley Lim <slim679975@gmail.com>
2023-08-26 16:13:11 -0700

    :wrench: chore(text-elements): Remove leftover code block for italics class

--- cirrus-docs-next/src/fonts/text-elements/index.tsx
+++ cirrus-docs-next/src/fonts/text-elements/index.tsx
@@ -214,12 +214,6 @@ export const TextElementsPage: React.FC<any> = (props) => {
                                 />
                             </div>
                             <div className="col-lg-6">
-                                <CodeBlock
-                                    code={`<p class="italics">The quick brown fox jumps over the lazy dog.</p>`}
-                                    language="htmlbars"
-                                />
-                            </div>
-                            <div className="col-lg-6">
                                 <p className="title mb-0">Caret</p>
                                 <p className="caret"></p>
                             </div>
