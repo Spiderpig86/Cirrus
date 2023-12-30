9d82a7b1c1b22b5990fd7990265914afd15885d8
Stanley Lim <slim679975@gmail.com>
2023-12-30 12:41:52 -0800

    :wrench: chore(index.ts): Update version

--- cirrus-docs-next/constants/index.ts
+++ cirrus-docs-next/constants/index.ts
@@ -1,6 +1,6 @@
 export const PAGE_TITLE_PREFIX = 'Cirrus -';
-export const VERSION = '0.7.1';
-export const VERSION_NAME = 'Gamma VII';
+export const VERSION = '0.7.2 Patch 1;
+export const VERSION_NAME = 'Gamma VIII';
 
 export const v1Colors = ['dark', 'primary', 'link', 'info', 'success', 'warning', 'danger'];
 export const DEFAULT_SIZING_SYSTEM = [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 24, 32, 48, 64];
