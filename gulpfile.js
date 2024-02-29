f745f2703973a68182d6a1312242e4e34576dfed
Stanley Lim <slim679975@gmail.com>
2024-02-29 01:38:22 -0800

    :sparkles: feat(next): Finish updating class generation for layout, update Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,22 @@ import size from 'gulp-size';
 const head =
     '/*\r\n* Cirrus ' +
     prop.version +
-    `\r\n* Stanley Lim, Copyright ${new Date().getFullYear()}\r\n* https://spiderpig86.github.io/Cirrus\r\n*/\r\n`;
+    `\r\n* Stanley Lim, Copyright ${new Date().getFullYear()}\r\n* https://cirrus-ui.com\r\n*/\r\n`;
 
 const environment = {
     isCi: process.env.CI ?? false, // Github actions default variable is always set to true
 };
 
-generateGulpBuild(`ext`, `src/cirrus-ext.scss`, `cirrus`);
-generateGulpBuild(`core`, `src/cirrus-core.scss`, `cirrus-core`);
-generateGulpBuild(`all`, `src/cirrus-all.scss`, `cirrus-all`);
+generateGulpBuild(`ext`, `src/cirrus-ext.scss`, `cirrus`, `./dist/`);
+generateGulpBuild(`core`, `src/cirrus-core.scss`, `cirrus-core`, `./dist/`);
+generateGulpBuild(`all`, `src/cirrus-all.scss`, `cirrus-all`, `./dist/`);
+
+// 0.8.0 beta builds
+generateGulpBuild(`next-all`, `next/src/cirrus-all.scss`, `cirrus-all`, `./next/dist/`);
 
 // source file name
 // file name
-function generateGulpBuild(taskName, sassFilePath, outputName) {
+function generateGulpBuild(taskName, sassFilePath, outputName, distDir) {
     gulp.task(taskName, () => {
         return gulp
             .src([sassFilePath])
@@ -47,7 +50,7 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
             .pipe(concat(`${outputName}.css`))
             .pipe(header(head))
             .pipe(size())
-            .pipe(gulp.dest('./dist/'))
+            .pipe(gulp.dest(distDir))
             .on('error', (err) => {
                 console.error(err);
                 process.exit(1);
@@ -59,7 +62,7 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
         gulp.series(taskName, () => {
             return (
                 gulp
-                    .src([`./dist/${outputName}.css`])
+                    .src([`${distDir}${outputName}.css`])
                     .pipe(
                         minify(
                             {
@@ -93,7 +96,7 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
                     .pipe(header(head))
                     .pipe(size())
                     .pipe(concat(`${outputName}.min.css`))
-                    .pipe(gulp.dest('./dist/'))
+                    .pipe(gulp.dest(distDir))
                     .on('error', (err) => {
                         console.error(`Error encountered for task ${taskName}. Failing.`);
                         process.exit(1);
@@ -108,7 +111,7 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
         gulp.series(taskName, `minify-${taskName}`, () => {
             return (
                 gulp
-                    .src([`./dist/${outputName}.min.css`])
+                    .src([`${distDir}${outputName}.min.css`])
                     .pipe(gzip())
                     .pipe(
                         sizereport({
@@ -116,7 +119,7 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
                             total: false,
                         })
                     )
-                    .pipe(gulp.dest('./dist/'))
+                    .pipe(gulp.dest(distDir))
                     .on('error', (err) => {
                         console.error(`Error encountered for task ${taskName}. Failing.`);
                         process.exit(1);
@@ -129,5 +132,6 @@ function generateGulpBuild(taskName, sassFilePath, outputName) {
 gulp.task('watch', () => gulp.watch('./src/**/*.scss', gulp.parallel('minify-ext', 'minify-core', 'minify-all')));
 
 gulp.task('default', gulp.parallel('minify-ext', 'minify-core', 'minify-all'));
+gulp.task('next', gulp.parallel('minify-next-all'));
 
 gulp.task('gzip', gulp.parallel('gzip-ext', 'gzip-core', 'gzip-all'));
